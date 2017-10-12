ec8980098b982c9efa2e2b26984a76a42906dc43
Isaac Rose <isaacdrose@gmail.com>
2017-10-12 16:26:00 -0400

    First commit.

---
+++ Sources/mqx_tasks.h
@@ -0,0 +1,80 @@
+/* ###################################################################
+**     Filename    : mqx_tasks.h
+**     Project     : Test
+**     Processor   : MK22FN512VLH12
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2017-10-12, 12:40, # CodeGen: 1
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Contents    :
+**         task_idle - void task_idle(uint32_t task_init_data);
+**
+** ###################################################################*/
+/*!
+** @file mqx_tasks.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup mqx_tasks_module mqx_tasks module documentation
+**  @{
+*/         
+
+#ifndef __mqx_tasks_H
+#define __mqx_tasks_H
+/* MODULE mqx_tasks */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "pin_mux.h"
+#include "MQX1.h"
+#include "SystemTimer1.h"
+#include "SM1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  task_idle (module mqx_tasks)
+**
+**     Component   :  Task1 [MQXLite_task]
+**     Description :
+**         MQX task routine. The routine is generated into mqx_tasks.c
+**         file.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         task_init_data  - 
+**     Returns     : Nothing
+** ===================================================================
+*/
+void task_idle(uint32_t task_init_data);
+
+
+/* END mqx_tasks */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __mqx_tasks_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
